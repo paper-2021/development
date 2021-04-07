cbf27782eb57312361ab7aa72bcef9cb831f0de4
ujin2021 <ujin2021@naver.com>
2021-04-07 22:04:09 +0900

    fix: Fix send image lambda function

--- rsu/imageSendToCloud.py
+++ rsu/imageSendToCloud.py
@@ -1,22 +1,39 @@
-import requests
 import json
+import sys
+import greengrasssdk
+import platform
+import os
+import logging
+
+# Setup logging to stdout
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+client = greengrasssdk.client("iot-data")
 
 def function_handler(event, context):
     imagePath = '/dest/Imgtest/1_test.jpg' # image 경로 수정 필요
-    url = 'http://3.35.184.173:8000/image' 
+    url = 'http://3.35.184.173:8000/image'
+    client.publish(topic="test/image", payload = 'Sent from AWS IoT GG Core 1')
     try:
+        client.publish(topic="test/image", payload = 'Sent from AWS IoT GG Core 2')
         import requests
+        client.publish(topic="test/image", payload = 'Import requests success')
         headers = {
             'accept': 'application/json'
         }
         files = {
             'file': open(imagePath, 'rb')
         }
+        client.publish(topic="test/image", payload = 'Set headers, files success')
         response = requests.post(url, headers=headers, files=files)
-        print (response.json())
+        client.publish(topic="test/image", payload = 'Post success')
+        # print (response.json())
+        client.publish(topic="test/image", payload = str(response.json()))
+        client.publish(topic="test/image", payload = 'Send Image success')
     except Exception as e :
-        print("Imgtest err : ", e)
-    return {
-        'statusCode' : 200,
-        'body' : json.dumps('Image upload success')
-    }
+        client.publish(topic="test/image", payload = 'Send image error')
+        client.publish(topic="test/image", payload = str(e))
+        logger.error('Failed to publish msg : ' + repr(e))
+        # print("Imgtest err : ", e)
+    return
