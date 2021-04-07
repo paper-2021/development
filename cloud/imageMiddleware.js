f48d067f1e206ecc74371c3eda65ae84bb374ba4
ujin2021 <ujin2021@naver.com>
2021-04-07 15:54:13 +0900

    feat: Add store image in cloud resource

---
+++ cloud/imageMiddleware.js
@@ -0,0 +1,27 @@
+const multer = require('multer')
+const moment = require('moment')
+const fs = require('fs')
+
+const makeDir = async(dir) => {
+    if(!fs.existsSync(dir)) {
+        fs.mkdirSync(dir)
+    }
+}
+
+const datetimeTight = async() => {
+    return moment().format('YYYYMMDDHHmmss')
+}
+
+const storage = multer.diskStorage({
+    destination: async function(req, file, cb) {
+        let type = file.originalname[0]
+        await makeDir('public/upload/' + type)
+        cb(null, 'public/upload/' + type) // file save path(backend/upload)
+    },
+    filename: async function(req, file, cb) {
+        console.log(file.originalname)
+        cb(null, await datetimeTight() + '_' + file.originalname) // 저장되는 file명
+    }
+})
+
+exports.upload = multer({ storage: storage }).single("file") // single : 하나의 file 업로드
