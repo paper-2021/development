02ba5f220d3e7fd6d125b528ee08294950819970
chea-young <63410509+chea-young@users.noreply.github.com>
2021-04-12 22:33:01 +0900

    feat: RSUState table 추가 및 조회 기능

--- rsu/create db_py.py
+++ rsu/create db_py.py
@@ -13,47 +13,56 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 client = greengrasssdk.client('iot-data')
 volumePath = '/src/LRAtest'
 # /dest/DB -> ~/DB
-
-    
-def function_handler(event, context):
-    client.publish(topic='DB/test', payload='Start function')
-    try:
-        client.publish(topic='DB/test', payload='Sent from AWS IoT Greengrass Core about DB')
-        volumeInfo = os.stat(volumePath)
-        client.publish(topic='DB/test', payload=str(volumeInfo))
-        
-        import sqlite3
-        client.publish(topic='DB/test', payload='check sqlite3 import')
-        
-        #conn = sqlite3.connect(volumePath + '/test.db')
-        #cur = conn.cursor()
-        #conn.close()
-        client.publish(topic='DB/test', payload='update DB')
-        
-        # 파이썬 파일 생성
-        with open(volumePath + '/create_db.py', 'w') as output:
-            output.write("""import sqlite3
+code =""" import sqlite3
 import sys
 
-sys.stdout = open('/src/LRAtest/output.txt', 'w')
+sys.stdout = open('output.txt', 'w')
 
-conn = sqlite3.connect('/src/LRAtest/check.db')
+conn = sqlite3.connect('check.db')
 cur = conn.cursor()
-cur.execute("CREATE TABLE RSU(rsu_id INTEGER PRIMARY KEY, rsu_ip TEXT);")
-cur.execute("CREATE TABLE NearRSU(rsu_id INTEGER PRIMARY KEY, FOREIGN KEY(rsu_id) REFERENCES RSU(rsu_id));")
-cur.execute("CREATE TABLE RSUState(state_id INTEGER PRIMARY KEY, rsu_id INTEGER, accident_type INTEGER, accident_size INTEGER, traffic INTEGER, FOREIGN KEY(rsu_id) REFERENCES RSU(rsu_id));")
-cur.execute("CREATE TABLE OBU(obu_id INTEGER PRIMARY KEY, destination TEXT);")
-print("1")
-cur.close()
-conn.close()""")
+
+try:
+    cur.execute("CREATE TABLE RSU(rsu_id INTEGER PRIMARY KEY, rsu_ip TEXT);")
+    cur.execute("CREATE TABLE NearRSU(rsu_id INTEGER PRIMARY KEY, FOREIGN KEY(rsu_id) REFERENCES RSU(rsu_id));")
+    cur.execute("CREATE TABLE RSUState(state_id INTEGER PRIMARY KEY, rsu_id INTEGER, accident_type INTEGER, accident_size INTEGER, traffic INTEGER, FOREIGN KEY(rsu_id) REFERENCES RSU(rsu_id));")
+    cur.execute("CREATE TABLE OBU(obu_id INTEGER PRIMARY KEY, destination TEXT);")
+    cur.close()
+    conn.close()
+except sqlite3.OperationalError:
+    print('table color already exist')
+
+# 데이터 베이스에 집어 넣는 프로그램
+def insert_db(table, data):
+    conn = sqlite3.connect('check.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO " + table + " VALUES " + data)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "Sucessfully insert the data in " + table
+
+def select_db(table):
+    conn = sqlite3.connect('check.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * from " + table)
+    return cur.fetchall()"""
+    
+def function_handler(event, context):
+    client.publish(topic='DB/test', payload='Start lambda function')
+    try:
+        # 파이썬 파일 생성
+        with open(volumePath + '/create_db.py', 'w') as output:
+            output.write(code)
         client.publish(topic='DB/test', payload="complete make db file")
         
         with open(volumePath + '/create_db.py', 'r') as myfile:
             data = myfile.read()
         client.publish(topic='DB/test', payload=data)
-        
         import create_db
-        
+
+        client.publish(topic='test/select/RSUState', payload=create_db.select_db('RSUState'))
+        client.publish(topic='test/insert/RSUState', payload=create_db.insert_db('RSUState', "(1, 1, 1, 50, 50)"))
+        client.publish(topic='test/select/RSUState', payload=create_db.select_db('RSUState'))
         
     except Exception as e:
         client.publish(topic='DB/test', payload='Error: '+str(e))
--- rsu/create_db.py
+++ rsu/create_db.py
@@ -6,10 +6,34 @@ sys.stdout = open('output.txt', 'w')
 
 conn = sqlite3.connect('check.db')
 cur = conn.cursor()
-cur.execute("CREATE TABLE RSU(rsu_id INTEGER PRIMARY KEY, rsu_ip TEXT);")
-cur.execute("CREATE TABLE NearRSU(rsu_id INTEGER PRIMARY KEY, FOREIGN KEY(rsu_id) REFERENCES RSU(rsu_id));")
-cur.execute("CREATE TABLE RSUState(state_id INTEGER PRIMARY KEY, rsu_id INTEGER, accident_type INTEGER, accident_size INTEGER, traffic INTEGER, FOREIGN KEY(rsu_id) REFERENCES RSU(rsu_id));")
-cur.execute("CREATE TABLE OBU(obu_id INTEGER PRIMARY KEY, destination TEXT);")
-print("1")
-cur.close()
-conn.close()
+
+try:
+    cur.execute("CREATE TABLE RSU(rsu_id INTEGER PRIMARY KEY, rsu_ip TEXT);")
+    cur.execute("CREATE TABLE NearRSU(rsu_id INTEGER PRIMARY KEY, FOREIGN KEY(rsu_id) REFERENCES RSU(rsu_id));")
+    cur.execute("CREATE TABLE RSUState(state_id INTEGER PRIMARY KEY, rsu_id INTEGER, accident_type INTEGER, accident_size INTEGER, traffic INTEGER, FOREIGN KEY(rsu_id) REFERENCES RSU(rsu_id));")
+    cur.execute("CREATE TABLE OBU(obu_id INTEGER PRIMARY KEY, destination TEXT);")
+    cur.close()
+    conn.close()
+except sqlite3.OperationalError:
+    print('table color already exist')
+
+# 데이터 베이스에 집어 넣는 프로그램
+def insert_db(table, data):
+    conn = sqlite3.connect('check.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO " + table + " VALUES " + data)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "Sucessfully insert the data in " + table
+
+def select_db(table):
+    conn = sqlite3.connect('check.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * from " + table)
+    return cur.fetchall()
+"""
+print(select_db('RSU'))
+print(insert_db('RSU', "(1234, '192.168.195')"))
+print(select_db('RSU'))
+"""
