b3fce1d93774a89b5b93af1a7cfb8c15a39a152e
chea-young <63410509+chea-young@users.noreply.github.com>
2021-06-02 22:02:28 +0900

    fix: db and test file

--- obu/db_obu.py
+++ obu/db_obu.py
@@ -1,33 +1,40 @@
 # -*- coding: utf-8 -*-
-# 데이터 베이스를 생성해서 output을 output.txt에 넣는 코드
 import sqlite3
+db_name = 'obu.sqlite3'
 
-conn = sqlite3.connect('check.db')
-cur = conn.cursor()
+def create_db() :
+    try :
+        con = sqlite3.connect(db_name)
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS RSU (link_id INTEGER PRIMARY KEY AUTOINCREMENT, start INTEGER, end INTEGER, start_latitude TEXT, start_longitude TEXT, end_latitude TEXT, end_longitude TEXT)")
+        print('create_db success')
+    except Exception as e :
+        print(e)
+        print('create_db failed')
+    finally :
+        con.close()
 
-try:
-    cur.execute("CREATE TABLE RSU(rsu_id INTEGER PRIMARY KEY, start INTEGER, end INTEGER, start_latitude TEXT, start_longitude TEXT, end_latitude TEXT, end_logitude TEXT,);")
-    cur.close()
-    conn.close()
-except sqlite3.OperationalError:
-    pass
+def select_start(obu_loc):
+    try:
+        obu_loc = obu_loc.split(',')
+        con = sqlite3.connect(db_name)
+        cur = con.cursor()
+        cur.execute("SELECT start from RSU where start_latitude>=%s and start_longitude>=%s and  end_latitude<=%s and end_longitude<=%s" %(obu_loc[0], obu_loc[1], obu_loc[0], obu_loc[1]))
+        return str(cur.fetchall())
+    except Exception as e:
+        print(e)
+        print('select_start failed')
+    finally:
+        con.close
 
-# 데이터 베이스에 집어 넣는 프로그램
-def insert_db(table, data):
-    cur.execute("INSERT INTO " + table + " VALUES " + data)
-    conn.commit()
-    cur.close()
-    conn.close()
-    return "Sucessfully insert the data in " + table
-
-def select_db_location(rsu_ip):
-    cur.execute("SELECT location from RSU where rsu_ip = " + rsu_ip)
-    return str(cur.fetchall())
-
-def select_db_rsu_id(obu_loc):
-    cur.execute("SELECT rsu_id from RSU where location = " + rsu_loc)
-    return str(cur.fetchall())
-
-def select_db_rsu_ip(obu_loc):
-    cur.execute("SELECT rsu_id from RSU where location = " + rsu_loc)
-    return str(cur.fetchall())
+def select_select_rsu_loc(rsu_id):
+    try:
+        con = sqlite3.connect(db_name)
+        cur = con.cursor()
+        cur.execute("SELECT start_latitude, start_longitude from RSU where start=%d" %(rsu_id))
+        return cur.fetchall()[0]
+    except Exception as e:
+        print(e)
+        print('select_start failed')
+    finally:
+        con.close
--- obu/html/map_traffic.js
+++ obu/html/map_traffic.js
@@ -1,33 +1,31 @@
 var map, marker;
-
+document.getElementById("u4_img").src = "http://3.35.184.173:8000/upload/3/20210507212215_3_accident.jpg";
 function initTmap(){
     var map = new Tmapv2.Map("map_div",  
     {
-        center: new Tmapv2.LatLng(37.261851,127.031121), // 수원시청을 중심으로
+        center: new Tmapv2.LatLng(37.495, 127.021), 
         width: "1500px", 
         height: "700px",
-        zoom: 16
+        zoom: 14
     });
-    // 마커 생성, 마커 이미지 유진이랑 상의하고 바꾸기
-    var rsu = new Tmapv2.Marker({
-        position: new Tmapv2.LatLng(37.261851,127.031121), //Marker의 중심좌표 설정, rsu_ip 가 들어가도록 하게 하기
-        map: map //Marker가 표시될 Map 설정..
-    });
-    //경로 그려주기, 화살표만들기 가능한지 해보기 45도로
+    
     var polyline = new Tmapv2.Polyline({
-        path: [
-            new Tmapv2.LatLng(37.262338, 127.029050), // 선의 꼭짓점 좌표
-            new Tmapv2.LatLng(37.261621, 127.031807), // 선의 꼭짓점 좌표
+        path: [new Tmapv2.LatLng(37.518, 127.050),
+            new Tmapv2.LatLng(37.513, 127.053)
         ],
-        strokeColor: "#dd00dd", // 라인 색상
-		strokeWeight: 6, // 라인 두께
-		draggable: true, //드래그 여부
-		strokeStyle:'dot', // 선의 종류 soild, dash,
-		outline: true, // 외각 선을 설정
-		outlineColor:'#ffffff', // 외각 선 색상
-		map: map // 지도 객체
+        strokeColor: "#dd00dd",
+		strokeWeight: 6,
+		draggable: true, 
+		strokeStyle:'dot', 
+		outline: true,
+		outlineColor:'#ffffff',
+		map: map 
     });
 
-    document.getElementById("u4_img").src = 'images/page_1/u4_div.png';
-
+    var car = new Tmapv2.Marker({
+        position: new Tmapv2.LatLng('37.518, 127.050'),
+        icon: "images/page_1/car.png", 
+        map: map 
+    });
 } 
+
